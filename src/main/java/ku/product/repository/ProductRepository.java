d9776de95a73bf3b758c6a51ce59db76cf8da88d
RyukungG <ryu.nosuke@hotmail.com>
2024-03-07 09:01:21 +0700

    first commit

---
+++ src/main/java/ku/product/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package ku.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository extends JpaRepository<Product, > {
+}
